a5c71e3911c5a362fa61e155a02eebdea984b153
Meirzhan <meiirzhan04@gmail.com>
2024-08-02 20:37:40 +0500

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -23,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={poppins.className}>
         <main>{children}</main>
+        <Analytics />
       </body>
     </html>
   );
